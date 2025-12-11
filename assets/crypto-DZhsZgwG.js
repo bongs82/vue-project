709a8b869983685cb02236ae8ffa191712815739
bongs82 <hyobong82@gmail.com>
2025-12-11 16:38:23 +0900

    Updates

--- assets/crypto-DZhsZgwG.js
+++
@@ -1,1 +0,0 @@
-async function h(r){const a=new TextEncoder().encode(r),s=await crypto.subtle.digest("SHA-256",a);return Array.from(new Uint8Array(s)).map(t=>t.toString(16).padStart(2,"0")).join("")}export{h};
